78aece61783a382e8909ddd82b506437f16aaefb
kjwilliamson <kami63116@yahoo.com>
2024-03-20 16:23:25 -0700

    first commit

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+      
+      
+    char sport = 'y';
+    char outdoor = 'n';
+    if(sport == 'n') {
+      if(outdoor == 'n') {
+        System.out.println("No sport.");
+      }
+    }
+    else {
+      if(outdoor == 'n') {
+        System.out.println("Indoor sport.");
+      }
+    else {
+        System.out.println("Outdoor sport.");
+      }
+    }
+    
+    
+    
+    }
+  }
+  
+  
